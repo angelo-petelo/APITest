d880e4fcdb7acc2957a24ee58ff7502aba63e76f
Nicholas Petelo <npetelo@uci.edu>
2020-06-22 20:52:42 -0700

    Test branch

--- APITest/ViewController.swift
+++ APITest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("HELLO")
+        print("BRANCH")
         // Do any additional setup after loading the view.
     }
 
